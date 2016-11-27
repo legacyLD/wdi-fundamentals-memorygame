d052b79bdecd6d336fd94ca621a2f187633a911d
Laura Dean <lauradean@MacBook-Pro-2.local>
2016-11-27 15:21:25 -0800

    added game logic for match/no match

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+if (cardOne==cardFour) {
+  alert("You found a match!")
+}
+else {
+  alert("Sorry, try again.")
+}
