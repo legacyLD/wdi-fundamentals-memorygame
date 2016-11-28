b6e55c30b3f8cbcbfa5ea7f33ce85a4291668d7b
Laura Dean <lauradean@MacBook-Pro-2.local>
2016-11-27 18:20:42 -0800

    added js to create cards, not sure I'm firiing it off properly yet (have to go into console to run function, as opposed to when it wasn't a function and just fired as soon as the page loaded, instructions not clear to me regarding 'firing js'

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,19 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
-if (cardOne==cardFour) {
+/*if (cardOne==cardFour) {
   alert("You found a match!")
 }
 else {
   alert("Sorry, try again.")
+}*/
+var gameBoard = document.getElementById('game-board');
+var now;
+var createCards = function(now){
+  for (var x = 0; x < 4; x += 1){
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    document.getElementById('game-board').appendChild(newCard);
+    console.log("woo");
+  }
 }
