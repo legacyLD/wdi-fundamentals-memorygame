b5669fc232192210386349d2d25faa775822dfa3
Laura Dean <lauradean@MacBook-Pro-2.local>
2016-11-27 21:59:32 -0800

    added more js logic, not totally working yet

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,41 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
-var cardFour = "queen";
-/*if (cardOne==cardFour) {
-  alert("You found a match!")
-}
-else {
-  alert("Sorry, try again.")
-}*/
+var cardFour = "queen";*/
 var gameBoard = document.getElementById('game-board');
 var now;
 var createCards = function(now){
-  for (var x = 0; x < 4; x += 1){
+  for (var x = 0; x < cards.length; x += 1){
     var newCard = document.createElement('div');
     newCard.className = 'card';
     document.getElementById('game-board').appendChild(newCard);
-    console.log("woo");
+    gameBoard.setAttribute('data-card', cards[i]);
+    }
+    console.log("woo!");
+  }
+var cards = ['queen', 'king', 'queen', 'king'];
+var cardsInPlay = [];
+for (var i = 0; i < cards.length; i++) {
+    gameBoard.addEventListener('click', isTwoCards);
+    gameBoard.addEventListener('click', showImg)
+}
+function showImg() {
+  gameBoard.innerHTML = '<img src="king.jpg" alt="King of Spades" />';
+}
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+
+  }
+}
+var isMatch = function(){
+  if (cardsInPlay[0]==cardsInPlay[1]) {
+    alert("You found a match!")
+  }
+  else {
+    alert("Sorry, try again.")
   }
 }
